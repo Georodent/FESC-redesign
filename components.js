634cd9e4338b6025d67ae35306f261fa058b3568
George R <rodeng@uw.edu>
2018-05-29 14:56:38 -0700

    Added components for footer

--- components.js
+++ components.js
@@ -2,3 +2,6 @@
   template: '<header><div class="flexcontainer"><img src="images/header.jpg" alt="FESC logo"><h1 id="logo-fesc">FESC</h1><ul><li class="home"><a href="index.html">Home</a></li><li class="facility"><a href="chart.html">Energy Charts</a></li><li class="education"><a href="education.html">Education</a></li><li class="industry"><a href="industry.html">Energy Industry</a></li></ul></div><h4 class="florida">FLORIDA ENERGY SYSTEMS CONSORTIUM</h4></header>'
 }) 
   
+    Vue.component('sections', {
+  template: '<section><form name="email" onsubmit="address()"><label>Please input your email.<input type="text" name="eMail"></label><input type="submit"></form><a href="#"><h4>CONTACT</h4></a><a href="#"><h4>SITE MAP</h4></a></section>'
+}) 
--- java.js
+++ java.js
@@ -5,8 +5,16 @@ function address(){
   var email = document.email.eMail.value
   alert("You are all signed up for our mailing list!!!")
 }
+
+// this is for the header component
 var aboutApp = new Vue({
   el: '#app',
+  data: { }
+});
+
+// this is for the footer component
+var app = new Vue({
+  el: '#app1',
   data: { }
 });
 
